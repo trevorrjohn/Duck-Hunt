e26b5e590e332e0c003318fbc88ba858b1d8f1b1
Trevor John <TrevorRJohn@gmail.com>
2011-11-26 21:40:30 -0500

    finished

--- duck.js
+++ duck.js
@@ -294,43 +294,6 @@ function init() {
   sprite.onload = mainMenu();
 }
 
-//Code from W3Schools.org
-function setCookie(c_name,value,exdays)
-{
-var exdate=new Date();
-exdate.setDate(exdate.getDate() + exdays);
-var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-document.cookie=c_name + "=" + c_value;
-}
-
-function getCookie(c_name)
-{
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-{
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
-    }
-  }
-}
-
-function checkCookie()
-{
-var username=getCookie("username");
-  if (username!=null && username!="")
-  {
-    return true;
-  }
-else
-  {
-    return false;
-  }
-}
-
 function mainMenu() {
   if(localStorage.username != null && localStorage.score != null && navigator.onLine) {
     var userArray = new Array();
@@ -342,7 +305,7 @@ function mainMenu() {
     while(userArray.length != 0){
       var user = userArray.pop();
       var scoreA = scoreArray.pop();
-      if(user != "" && scoreA != ""){
+      if(user != "" || scoreA != ""){
         $.post("http://score-app.heroku.com/api/v1/scores", {game: "Duck-Hunt", score: scoreA*10, username: user});
       }
     }
